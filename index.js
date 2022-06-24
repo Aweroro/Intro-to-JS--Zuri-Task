4878e9db4c1b9e04018a27ed94923bb9f10a735a
Aweroro Hadi <60360809+Aweroro@users.noreply.github.com>
2022-06-24 18:40:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+var name = window.prompt('What is your name?');
+var height = window.prompt('What is your height (in inches)?')
+var country = window.prompt('What country are you from?')
+document.write('Hello ' + name + ', you are '  + height + ' inches tall and you are from ' + country );
